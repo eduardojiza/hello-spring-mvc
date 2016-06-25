c3abac9d05ca62bfef2fe3f170c7025c5695bd05
Eduardo Jimenez <eduardo@MacBook-Pro-de-Eduardo.local>
2016-06-25 13:00:08 -0500

    Inicio de proyecto

---
+++ src/main/java/com/whileapp/hello/spring/mvc/WebConfig.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.whileapp.hello.spring.mvc;
+
+/**
+ *
+ * @author eduardo
+ */
+public class WebConfig {
+    
+}
